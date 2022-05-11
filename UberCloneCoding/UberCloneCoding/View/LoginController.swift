9b8e801b5a68deb085f4ea75a6b3a3bcda634f32
neuli <asdfz888@naver.com>
2022-05-11 14:03:33 +0900

    set rootViewController

---
+++ UberCloneCoding/UberCloneCoding/View/LoginController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginController.swift
+//  UberCloneCoding
+//
+//  Created by neuli on 2022/05/11.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
