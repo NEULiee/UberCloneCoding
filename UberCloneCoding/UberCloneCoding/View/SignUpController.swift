8d300f00f2e785eece639b47e843169776cc896e
neuli <asdfz888@naver.com>
2022-05-11 16:10:15 +0900

    add SignUpController

---
+++ UberCloneCoding/UberCloneCoding/View/SignUpController.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpController.swift
+//  UberCloneCoding
+//
+//  Created by neuli on 2022/05/11.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - Selectors
+}
